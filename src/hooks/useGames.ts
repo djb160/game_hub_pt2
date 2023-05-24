8ebe79e1f2064ff4c9223d80060ac6162974d4c0
djb160 <dalebeatson@live.com>
2023-05-25 11:46:37 +1200

    simplified time calculations with ms

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,8 @@ import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/apiClient";
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { Platform } from "./usePlatforms";
+import ms from 'ms';
+
 
 
 const apiClient = new APIClient<Platform>("/games");
@@ -32,7 +34,7 @@ const useGames = (gameQuery: GameQuery) => {
             getNextPageParam: (lastPage, allPages) => {
                 return lastPage.next ? allPages.length + 1 : undefined;
             },
-            staleTime: 24 * 60 * 60 * 1000, // 24 Hours
+            staleTime: ms('24h'),
         })
     )
 }
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
-// import { useQuery } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import APIClient from "../services/apiClient";
+import ms from 'ms';
 
 
 const apiClient = new APIClient<Genre>("/genres");
@@ -17,7 +17,7 @@ const useGenres = () => {
         useQuery({
             queryKey: ["genres"],
             queryFn: apiClient.getAll,
-            staleTime: 24 * 60 * 60 * 1000, // 24 hours
+            staleTime: ms('24h'),
             initialData: genres,
         })
     )
